a6dd36a6f2b04bb7a1709641317e1e5ced4118dd
Bilous_EVG <zenia2003725@gmail.com>
2024-10-14 19:47:25 +0300

    20241014

---
+++ 20241014/0/name.py
@@ -0,0 +1,2 @@
+n = input().split()
+print(', '.join([n[-1], n[1], n[0]]))
---
+++ 20241014/0/sin.py
@@ -0,0 +1,18 @@
+from math import *
+
+def scale(a, b, A, B, x):
+    return (x - a) * (B - A) / (b - a) + A
+
+def screen(n):
+    print('\n'.join(''.join(row) for row in n))
+
+A, B = -5, 5
+W, H = 60, 25
+n = [[' '] * W for _ in range(H)]
+for i in range(W):
+    x = scale(0, W - 1, A, B, i)
+    y = sin(x)
+    s = round(scale(-1, 1, 0, H - 1, y))
+    n[s][i] = '@'
+
+screen(n)
